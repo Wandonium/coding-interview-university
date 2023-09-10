6f719bcc37053dc24f5855541a7b62c8c89c8520
Wandonium <hillarywando@gmail.com>
2023-09-10 12:51:29 +0300

    Python solution to problem complete.

--- exercises/file_handling/solutions/Solution.py
+++ exercises/file_handling/solutions/Solution.py
@@ -1,9 +1,19 @@
+nums = []
 with open('numbers.txt', 'r') as reader:
     # Read and print the entire file line by line
-    line = reader.readline()
+    # line = reader.readline()
+    # count = 0
+    # while line != '':   # The EOF char is an empty string
+    #     count += 1
+    #     num = int(line) * count 
+    #     print(num, end='\n')
+    #     line = reader.readline()
     count = 0
-    while line != '':   # The EOF char is an empty string
+    for line in reader:
         count += 1
-        num = int(line) * count 
+        num = int(line) * count
+        nums.append(num)
         print(num, end='\n')
-        line = reader.readline()
+with open('numbersPythonResult.txt', 'w') as writer:
+    for num in nums:
+        writer.write(str(num) + '\n')
